a09ef5e783c5dd1ec9c02d69a2efcfb85f85c613
Yehor Remizov <Yehor_Remizov@epam.com>
2019-03-20 19:25:51 +0300

    work in progress. Add eslint, stylelint, babel, react

---
+++ config/webpack/webpack.production.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new ExtractTextPlugin('css/[hash].style.css'),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Document'
+    })
+  ],
+  output: {
+    filename: 'js/[hash].bundle.js',
+    path: path.resolve(__dirname, '../../dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {name: 'assets/images/[name].[ext]'}
+          }
+          
+        ],
+        
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {name: 'assets/fonts/[name].[ext]'}
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+};
